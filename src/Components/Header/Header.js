12472f3cab8fc941f5729c29b62653dadf3828f8
Debabrata Saha <sahadebabrata570@gmail.com>
2021-10-12 11:35:43 +0600

    header component create and image added

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../../images/logo.png'
+const Header = () => {
+    return (
+        <div>
+            <h2>this is header component</h2>
+            <img src={logo} alt="" />
+        </div>
+    );
+};
+
+export default Header;
