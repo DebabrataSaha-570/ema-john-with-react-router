1582f31332a1a437d16c7b6a09956ca45eb488d2
Debabrata Saha <sahadebabrata570@gmail.com>
2021-10-12 11:52:02 +0600

    header component done and shop component created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header/Header';
+import Shop from './Components/Shop/Shop';
 
 function App() {
   return (
     <div >
       <Header></Header>
+      <Shop></Shop>
     </div>
   );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import logo from '../../images/logo.png'
+import './Header.css'
 const Header = () => {
     return (
-        <div>
-            <h2>this is header component</h2>
+        <div className='Header'>
             <img src={logo} alt="" />
+            <nav>
+                <a href="/shop">Shop</a>
+                <a href="/">Order Review</a>
+                <a href="">Manage Inventory</a>
+            </nav>
         </div>
     );
 };
