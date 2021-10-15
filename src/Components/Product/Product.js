c65b07d9a0eaff7c9c1772660497228e943e2559
Debabrata Saha <sahadebabrata570@gmail.com>
2021-10-15 12:45:00 +0600

    dynamic rating added

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -3,12 +3,13 @@ import './Product.css'
 import ReactDOM from 'react-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import Rating from 'react-rating';
 
 
 const Product = (props) => {
-    const { name, img, stock, seller, price } = props.product;
+    const { name, img, stock, seller, price, star } = props.product;
     const cartIcon = <FontAwesomeIcon icon={faShoppingCart} />
-    // console.log(props)
+    console.log(props)
     return (
         <div className="product" >
             <img src={img} alt="" />
@@ -17,6 +18,13 @@ const Product = (props) => {
                 <p><small>by: {seller}</small></p>
                 <p>Price: {price}</p>
                 <p><small>only {stock} left in stock - order soon</small></p>
+                <Rating
+                    emptySymbol="far fa-star"
+                    fullSymbol="fas fa-star icon-color"
+                    readonly
+                    initialRating={star}
+                > </Rating>
+                <br />
                 <button onClick={() => props.handleAddToCart(props.product)} className="btn-regular"> {cartIcon} add to cart</button>
             </div>
         </div>
