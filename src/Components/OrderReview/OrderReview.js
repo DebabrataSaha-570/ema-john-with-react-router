bec67b51aa44292e590ad1d03080c62e8039582c
Debabrata Saha <sahadebabrata570@gmail.com>
2021-10-24 00:42:25 +0600

    implemented review item and applied removed item handler

--- src/Components/OrderReview/OrderReview.js
+++ src/Components/OrderReview/OrderReview.js
@@ -2,15 +2,24 @@ import React from 'react';
 import useCart from '../../Hooks/useCart';
 import useProducts from '../../Hooks/useProduct';
 import Cart from '../Cart/Cart';
+import ReviewItem from '../ReviewItem/ReviewItem';
 
 const OrderReview = () => {
     const [products, setProducts] = useProducts()
-    const [cart] = useCart(products)
+    const [cart, setCart] = useCart(products)
+    const handleRemove = key => {
+        const newCart = cart.filter(product => product.key !== key);
+        setCart(newCart)
+    }
     return (
         <div className="shop-container">
             <div className="product-container">
                 {
-
+                    cart.map(product => <ReviewItem
+                        key={product.key}
+                        product={product}
+                        handleRemove={handleRemove}
+                    ></ReviewItem>)
                 }
             </div>
             <div className="cart-container">
--- src/Hooks/useCart.js
+++ src/Hooks/useCart.js
@@ -20,7 +20,7 @@ const useCart = products => {
             setCart(storedCart)
         }
     }, [products])
-    return [cart]
+    return [cart, setCart]
 }
 
 export default useCart;
